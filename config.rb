0f76b5fd36627c695622f69b7a929af466a22fd4
Jordan Wade <jjwade31@gmail.com>
2015-02-18 13:59:45 -0500

    Update comments and add livereload

--- config.rb
+++ config.rb
@@ -1,14 +1,25 @@
+# ====================================
+#   Activate Plugins
+# ====================================
+
 activate :automatic_image_sizes
 activate :directory_indexes
-# activate :livereload
+activate :livereload
 activate :automatic_image_sizes
 
-set :css_dir, 'assets/stylesheets'
-set :js_dir, 'assets/javascripts'
-set :images_dir, 'assets/images'
-set :fonts_dir, 'assets/fonts'
+# ====================================
+#   Global Variables
+# ====================================
 
-# Build-specific configuration
+set :css_dir,     'assets/stylesheets'
+set :js_dir,      'assets/javascripts'
+set :images_dir,  'assets/images'
+set :fonts_dir,   'assets/fonts'
+
+# ====================================
+#   Build Configuration
+# ====================================
+
 configure :build do
   activate :minify_css
   activate :minify_javascript
